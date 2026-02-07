ba908fcbb759e81201bfa48f4d7705af17e904f4
cracker1010987-create <cracker1010987@gmail.com>
2026-02-07 15:26:54 +0900

    [Silver V] Title: 피보나치 수 4, Time: 52 ms, Memory: 36964 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+if n <= 1:
+    print(n)
+else:
+    arr = [0] * n
+    arr[0] = 1
+    arr[1] = 1
+    for i in range(2, n):
+        arr[i] = arr[i - 1] + arr[i - 2]
+    print(arr[-1])
