6c7a1892693278a12c77bafff2e8d85a4309c88c
cracker1010987-create <cracker1010987@gmail.com>
2026-03-15 11:14:32 +0900

    [Silver IV] Title: 암기왕, Time: 1412 ms, Memory: 224392 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.py
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -1,23 +1,14 @@
 t = int(input())
 for i in range(t):
     n = int(input())
-    arr1 = list(map(int,input().split()))
-    arr1.sort()
+    arr1 = set(map(int,input().split()))
     m = int(input())
     arr2 = list(map(int,input().split()))
 
     for j in range(m):
-        left, right = 0, n - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if arr1[mid] > arr2[j]:
-                right = mid - 1
-            elif arr1[mid] < arr2[j]:
-                left = mid + 1
-            else:
-                print(1)
-                break
+        if arr2[j] in arr1:
+            print(1)
         else:
             print(0)
 
-# t * (n + m) * log n
+# t * m
