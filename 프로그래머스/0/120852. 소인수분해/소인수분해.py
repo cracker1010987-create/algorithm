cd794190a39e175612a9c2520787205daa7bdfe9
cracker1010987-create <cracker1010987@gmail.com>
2026-05-15 23:32:51 +0900

    [level 0] Title: 소인수분해, Time: 0.37 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = []
+    for i in range(2, n + 1):
+        if n % i == 0:
+            if i not in answer:
+                answer.append(i)
+            while n % i == 0:
+                n = n // i
+        if n == 1:
+            break
+            
+    return answer
